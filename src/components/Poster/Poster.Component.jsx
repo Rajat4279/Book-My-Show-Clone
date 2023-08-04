ccca52d5140c80bc3a4922e3036904bdffd78140
Rajat4279 <rajatsrivastava4279@gmail.com>
2023-08-04 19:06:27 +0530

    Added Some more plays

--- src/components/Poster/Poster.Component.jsx
+++ src/components/Poster/Poster.Component.jsx
@@ -2,62 +2,61 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const MoviePoster = (props) => {
-  <Link to={`/movie/${props.id}`}>
-    <div className="flex flex-col items-start gap-3 px-1 md:px-3">
-      <div className="h-40 md:h-80">
-        <img
-          src={`https://image.tmdb.org/t/p/original${props.poster_path}`}
-          alt="poster"
-          className="w-full h-full rounded-md"
-        />
-      </div>
-      <h3
-        className={`text-lg font-bold ${
-          props.isDark ? "text-white" : "text-gray-800"
-        }`}
-      >
-        {props.title}
-      </h3>
-    </div>
-  </Link>;
-};
-
-const PlayPoster = (props) => {
   return (
-    <>
-      <div className="flex flex-col items-start gap-3 px-1 md:px-3">
+    <Link to={`/movie/${props.id}`}>
+      <div className="flex flex-col items-start gap-2 px-1 md:px-3">
         <div className="h-40 md:h-80">
           <img
-            src={props.playSrc}
+            src={`https://image.tmdb.org/t/p/original${props.poster_path}`}
             alt="poster"
             className="w-full h-full rounded-md"
           />
         </div>
         <h3
           className={`text-lg font-bold ${
-            props.isDark ? "text-white" : "text-gray-800"
+            props.isDark ? "text-white" : "text-gray-700"
           }`}
         >
           {props.title}
         </h3>
-        <p
-          className={`text-md font-semibold ${
-            props.isDark ? "text-white" : "text-gray-700"
-          }`}
-        >
-          {props.subtitle}
-        </p>
       </div>
-    </>
+    </Link>
+  );
+};
+
+const PlayPoster = (props) => {
+  return (
+    <div className="flex flex-col items-start gap-2 px-1 md:px-3">
+      <div className="h-40 md:h-80">
+        <img
+          src={props.playSrc}
+          alt="poster"
+          className="w-full h-full rounded-md object-cover object-center"
+        />
+      </div>
+      <h3
+        className={`text-lg font-bold ${
+          props.isDark ? "text-white" : "text-gray-700"
+        }`}
+      >
+        {props.title}
+      </h3>
+      <p
+        className={`text-md font-semibold ${
+          props.isDark ? "text-white" : "text-gray-700"
+        }`}
+      >
+        {props.subtitle}
+      </p>
+    </div>
   );
 };
 
 const Poster = (props) => {
-  if (props.playSrc) {
+  if (props.isPlay) {
     return <PlayPoster {...props} />;
-  } else {
-    return <MoviePoster {...props} />;
   }
+  return <MoviePoster {...props} />;
 };
 
 export default Poster;
--- src/pages/Play.page.jsx
+++ src/pages/Play.page.jsx
@@ -17,6 +17,7 @@ const PlayPage = () => {
             <div className="flex flex-wrap">
               <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
                 <Poster
+                  isPlay={true}
                   playSrc="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U3VuLCA2IEF1Zw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00329412-sufnzfqqbu-portrait.jpg"
                   title="Kal Ki Chinta Nahi Karta ft. Ravi Gupta"
                   subtitle="Comedy Shows | Hindi | 16yrs + | 1hr 15mins"
@@ -24,30 +25,34 @@ const PlayPage = () => {
               </div>
               <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
                 <Poster
-                  playSrc="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U3VuLCA2IEF1Zw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00329412-sufnzfqqbu-portrait.jpg"
-                  title="Kal Ki Chinta Nahi Karta ft. Ravi Gupta"
-                  subtitle="Comedy Shows | Hindi | 16yrs + | 1hr 15mins"
+                  isPlay={true}
+                  playSrc="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U2F0LCA1IEF1Zw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00362597-tbzgjbwmxc-portrait.jpg"
+                  title="Gaurav Kapoor Live - Standup comedy show"
+                  subtitle="Comedy | Hindi | 16yrs + | 1hr 15mins"
                 />
               </div>
               <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
                 <Poster
-                  playSrc="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U3VuLCA2IEF1Zw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00329412-sufnzfqqbu-portrait.jpg"
-                  title="Kal Ki Chinta Nahi Karta ft. Ravi Gupta"
-                  subtitle="Comedy Shows | Hindi | 16yrs + | 1hr 15mins"
+                  isPlay={true}
+                  playSrc="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-RnJpLCA0IEF1Zw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00362432-qcppkrguzf-portrait.jpg"
+                  title="Open Mic : Comedy,Poetry,..."
+                  subtitle="Comedy, Musical | Hindi, Engl..."
                 />
               </div>
               <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
                 <Poster
-                  playSrc="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U3VuLCA2IEF1Zw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00329412-sufnzfqqbu-portrait.jpg"
-                  title="Kal Ki Chinta Nahi Karta ft. Ravi Gupta"
-                  subtitle="Comedy Shows | Hindi | 16yrs + | 1hr 15mins"
+                  isPlay={true}
+                  playSrc="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U2F0LCAyMCBKYW4%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00357347-wlwdbteuap-portrait.jpg"
+                  title="Kids Sudoku Championsh..."
+                  subtitle="Education | English | 3yrs..."
                 />
               </div>
               <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
                 <Poster
-                  playSrc="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U3VuLCA2IEF1Zw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00329412-sufnzfqqbu-portrait.jpg"
-                  title="Kal Ki Chinta Nahi Karta ft. Ravi Gupta"
-                  subtitle="Comedy Shows | Hindi | 16yrs + | 1hr 15mins"
+                  isPlay={true}
+                  playSrc="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-RnJpLCA0IEF1Zw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00363421-kvqfsuxwel-portrait.jpg"
+                  title="yourMICSTERS comedy open mic - 10"
+                  subtitle="Comedy, Musical | Hindi, English | 18yrs + | 1hr"
                 />
               </div>
             </div>
